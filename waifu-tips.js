cfd52cac620c84be53ad7b45ad6fe565aac0c12f
Han_Jing <88872267+es130172084@users.noreply.github.com>
2021-12-19 15:55:35 +0800

    Update waifu-tips.js

--- waifu-tips.js
+++ waifu-tips.js
@@ -20,15 +20,6 @@ function loadWidget(config) {
 	document.body.insertAdjacentHTML("beforeend", `<div id="waifu">
 			<div id="waifu-tips"></div>
 			<canvas id="live2d" width="700" height="700"></canvas>
-			<div id="waifu-tool">
-				<span class="fa fa-lg fa-comment"></span>
-				<span class="fa fa-lg fa-paper-plane"></span>
-				<span class="fa fa-lg fa-user-circle"></span>
-				#<span class="fa fa-lg fa-street-view"></span>
-				#<span class="fa fa-lg fa-camera-retro"></span>
-				<span class="fa fa-lg fa-info-circle"></span>
-				<span class="fa fa-lg fa-times"></span>
-			</div>
 		</div>`);
 	// https://stackoverflow.com/questions/24148403/trigger-css-transition-on-appended-element
 	setTimeout(() => {
